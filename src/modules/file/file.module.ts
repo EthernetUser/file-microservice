74ab46543d7d5bbe3ce99bcb3e47dd88a1cee926
Alexander Melnikov <okman5757@gmail.com>
2021-12-14 14:34:33 +0300

    add filemodule

---
+++ src/modules/file/file.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { FileController } from './file.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { File, FileSchema } from '../../schemas/file.schema';
+import { FileService } from './file.service';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: File.name,
+        schema: FileSchema,
+      },
+    ]),
+  ],
+  controllers: [FileController],
+  providers: [FileService],
+})
+export class FileModule {}
