c10b4adaaac2c8003d1fe922bdf622ba6bd4a700
Alexander Melnikov <okman5757@gmail.com>
2021-12-14 16:16:56 +0300

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./modules/app/app.module";
-import { ClusterService } from "./modules/cluster/cluster.service";
-import { Logger } from "@nestjs/common";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './modules/app/app.module';
+import { ClusterService } from './modules/cluster/cluster.service';
+import { Logger } from '@nestjs/common';
 
 const logger = new Logger('BOOTSTRAP');
 async function bootstrap() {
--- src/modules/file/file.module.ts
+++ src/modules/file/file.module.ts
@@ -3,7 +3,7 @@ import { FileController } from './file.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { File, FileSchema } from '../../schemas/file.schema';
 import { FileService } from './file.service';
-import { ScheduleModule } from "@nestjs/schedule";
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
