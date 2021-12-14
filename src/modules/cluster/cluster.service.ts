de475f740129633285c195aeaecacb11ef447b27
Alexander Melnikov <okman5757@gmail.com>
2021-12-14 16:35:52 +0300

    refactor

--- src/modules/cluster/cluster.service.ts
+++ src/modules/cluster/cluster.service.ts
@@ -8,13 +8,11 @@ export class ClusterService {
   private static readonly logger = new Logger('ClusterService');
   static clusterize(callback: () => void): void {
     if (cluster.isPrimary) {
-      console.log();
       this.logger.log(`Primary server started on ${process.pid}`);
       for (let i = 0; i < 3; i++) {
         cluster.fork();
       }
       cluster.on('exit', (worker) => {
-        console.log();
         this.logger.warn(`Worker ${worker.process.pid} died. Restarting...`);
         cluster.fork();
       });
